24f5a13f084645fd50a275c0c6b72932c1e0556c
psterner <psterner@hotmail.com>
2023-10-03 21:31:36 +0200

    Change bgColor when toggling radio btns

--- weeks/week-2/exercises/exercise-1/index.js
+++ weeks/week-2/exercises/exercise-1/index.js
@@ -1,39 +1,37 @@
-// Select the element with the class "box" and store it in the "box" variable.
-const box = document.querySelector(".box");
+// DOM Manipulation Exercise
 
-// Select all input elements with the name attribute set to "theme" and store them in the "themeRadios" variable.
-const themeRadios = document.querySelectorAll('input[name="theme"]');
+console.log("Start Here 😉");
 
-// Loop through each radio button in the "themeRadios" NodeList.
-themeRadios.forEach((radio) => {
-  // Add an event listener to each radio button to listen for the "change" event.
-  // This event triggers when the radio button's state changes (i.e., when it's selected).
-  radio.addEventListener("change", function () {
-    let color; // Declare a variable to store the background color value.
+/**
+ * 1. Target the div container element with the class of "box"
+ * 2. change the background color of this element once the user
+ * selects a theme from the theme options
+ */
 
-    // Check the value of the currently selected radio button (using 'this') and set the "color" variable accordingly.
-    if (this.value === "dark") {
-      color = "darkgray";
-    } else if (this.value === "light") {
-      color = "lightblue";
-    } else if (this.value === "crazy") {
-      color = "purple";
-    } else {
-      color = "lightgreen"; // Default color.
+// Target the box
+const boxElement = document.querySelector(".box");
+const pElement = boxElement.firstElementChild;
+pElement.style.color = "white";
+
+// Get all input radios, three. The first is default
+const radioButtons = document.querySelectorAll("input[type=radio]");
+
+// Loop through radio buttons and add eventlisteners
+radioButtons.forEach((radioButton) => {
+  radioButton.addEventListener("change", () => {
+    // Change the background color on box element
+    switch (radioButton.id) {
+      case "dark":
+        boxElement.style.backgroundColor = "#3b553b";
+        break;
+      case "light":
+        boxElement.style.backgroundColor = "rgb(141, 198, 141)";
+        break;
+      case "crazy":
+        boxElement.style.backgroundColor = "rgb(168, 21, 138)";
+        break;
+      default:
+        boxElement.style.backgroundColor = "lightgreen";
     }
-
-    // Add the "color-transition" class to the "box" element.
-    // This class presumably contains CSS that provides a smooth color transition effect.
-    box.classList.add("color-transition");
-
-    // Change the background color of the "box" element to the value stored in the "color" variable.
-    box.style.backgroundColor = color;
-
-    // Set a timeout to execute a function after a delay of 300 milliseconds.
-    setTimeout(() => {
-      // After the delay, remove the "color-transition" class from the "box" element.
-      // This might be done to ensure that subsequent changes to the box's style don't have the transition effect.
-      box.classList.remove("color-transition");
-    }, 300); // 300 milliseconds delay.
   });
 });
