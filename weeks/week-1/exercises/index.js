528ae134ba0ad24934083da3a3e6a6bf4935f441
AntonellaMorittu <antonellamorittu@gmail.com>
2023-10-03 13:16:05 +0200

    Fix CORS issue

--- weeks/week-1/exercises/index.js
+++ weeks/week-1/exercises/index.js
@@ -6,6 +6,9 @@ Where Technigo Cafe is replaced by the name of your cafe that is stored in the v
 */
 
 const cafeName = "Technigo Cafe";
+
+// TEMPLATE LITERALS === backtick approach’
+
 console.log(`Welcome to ${cafeName}! What would you like to order today?`);
 
 /*
@@ -15,9 +18,22 @@ Create a variable that stores how many coffees the customer wants.
 Print out the total price such as "There you go, that'll be 10 euros"
 Where 10 is replaced by the calculation of the total price.
 */
+
 // Good practice Approach
 
-// Bad practive Approach
+let coffeePrice = 335; 
+// Aparently super expensive coffee!
+
+const numberOfCoffees = 10;
+
+let totalPrice = coffeePrice * numberOfCoffees;
+let message = `There you go, that'll be ${totalPrice} KR`;
+console.log(message);
+
+// Bad practice Approach
+
+let myStatment = `There you go, that'll be ${coffeePrice * numberOfCoffees} KR`;
+console.log(myStatment);
 
 /*
 3)
@@ -25,6 +41,11 @@ Create a variable that stores a boolean.
 Print out "You said this coffee is the best, that was actually true"
 Where true is replaced by your varible
 */
+
+let isBestCofee = true;
+console.log(
+  `You said this coffee is the best, that was actually ${isBestCofee}`
+);
 
 /*
 4)
@@ -35,7 +56,15 @@ assign it a new value.
 Print it out. => This should give you the new value.
 */
 
+let cafeGuests = 10;
+console.log(cafeGuests);
+cafeGuests = false;
+console.log(cafeGuests);
+cafeGuests = ["I", "love", "Hip Hop"];
+console.log(cafeGuests);
+
 /*
+
 5)
 Create a variable called maxGuests, that shows us how many guests are allowed in the cafe.
 Assign it a value.
@@ -44,17 +73,26 @@ assign it a new value.
 Print it out. => This should give you an error because it shouldn't be able to be changed.
 */
 
+const maxGuests = 10;
+console.log(maxGuests);
 //maxGuests = "I also love rock n roll!";
+
 /*
 6)
 Create a variable that stores a string.
 Print out that string in only UPPERCASE letters.
 */
+let favoriteBand = "black sabbath";
+console.log(favoriteBand);
+let upperCase = favoriteBand.toUpperCase();
+console.log(upperCase);
 
 /*
 7)
 Print out the same string in only lowercase letters.
 */
+
+console.log(upperCase.toLowerCase());
 
 /*
 8) **BONUS**
@@ -63,3 +101,7 @@ Then figure out a way to replace the word "summer" in the string with the word "
 Should give you => "Today we have a special winter deal!"
 (check for a specific string method...)
 */
+
+let specialDeal = "Today we have a special summer deal!";
+let updatedDeal = specialDeal.replace("summer", "winter");
+console.log(updatedDeal);
