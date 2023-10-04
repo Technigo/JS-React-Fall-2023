a8f8a910d15f13e6e21257778ee410a8e3f24e30
psterner <psterner@hotmail.com>
2023-10-04 10:34:08 +0200

    Create a function that will compare user answer to right answer

--- weeks/week-2/exercises/exercise-2/index.js
+++ weeks/week-2/exercises/exercise-2/index.js
@@ -9,19 +9,18 @@ When you've set that up, invoke the function to see if it works.
 console.log("Iteration 01");
 const theCoders = (driver, navigator) => {
   console.log(
-    `The awesome coders for today is the driver ${driver} and the navigator ${navigator}`
+    `The awesome coders for today is the the driver ${driver} and the navigator ${navigator}`
   );
 };
-theCoders("nella", "tom");
+// theCoders("Nella", "Tom");
 console.log("----");
-
 /*
   2)
   Pass in only ONE name when you call the function and see what happens.
   (HINT, you should get undefined...)
   */
-console.log("Iteration 02");
-console.log("This Iteration is to showcase an undefined value on the console");
+// console.log("Iteration 02");
+// theCoders("Nella");
 console.log("----");
 /*
   3) 
@@ -30,20 +29,19 @@ console.log("----");
   or if the names ar the same length "The driver and the navigator has equally long names"
   */
 console.log("Iteration 03");
-const coderComparison = (driver, navigator) => {
-  let driverLength = driver.length;
-  let navLength = navigator.length;
-  if (driverLength > navLength) {
-    console.log("The driver has the longest name.");
-  } else if (driverLength < navLength) {
-    console.log("The navigator has the longest name.");
-  } else {
-    console.log("The driver and the navigator have equally long names.");
+const longestName = (driver, navigator) => {
+  switch (true) {
+    case driver.length > navigator.length:
+      console.log("The driver has the longest name.");
+      break;
+    case driver.length < navigator.length:
+      console.log("The navigator has the longest name");
+      break;
+    default:
+      console.log("The driver and the navigator has equally long names");
   }
 };
-coderComparison("nella", "tom");
-coderComparison("nella", "michelangelo");
-coderComparison("mathilda", "nella");
+// longestName("Nella", "Tom");
 console.log("----");
 
 /*
@@ -56,117 +54,41 @@ console.log("----");
   or alert('23! Younger than Zlatan') or something like that.
   Add as many conditonals as you want.
   */
-
 console.log("Iteration 04");
-
-console.log("---- Version 01");
 console.log("Using if-else-if-else statements");
-let tellMeYourAge = () => {
-  let userAge = prompt("How old are you? - Regular Conditional");
-  console.log(userAge);
-  if (userAge === 21) {
-    console.log(`You are 21 years old! - to be young`);
-  } else if (userAge >= 21 && userAge <= 30) {
-    console.log(`Enjoy your 20's - :)`);
-  } else if (userAge >= 31 && userAge <= 80) {
-    console.log(`Always try to stay active both physically and mentally!`);
-  } else if (userAge < 20) {
-    console.log(`Enjoy your early School years`);
+const tellMeYourAge = () => {
+  let userAge = prompt("How old are you?");
+  if (userAge <= 35) {
+    console.log(`wow you are ${userAge}! that's young!`);
+  } else if (userAge > 35 && userAge <= 60) {
+    console.log(`wow you are ${userAge}! that's awesome!`);
   } else {
-    console.log(`Sit back and relax!`);
-  }
-};
-//tellMeYourAge();
-
-console.log("---- Version 02");
-console.log("Using switch");
-let tellMeYourAgeSwitch = () => {
-  let userAge = prompt("How old are you? - Using Switch Statement");
-  console.log(userAge);
-
-  switch (parseInt(userAge)) {
-    case 21:
-      console.log(`You are 21 years old! - to be young`);
-      break;
-    case userAge >= 21 && userAge <= 30:
-      console.log(`Enjoy your 20's - :)`);
-      break;
-    case userAge >= 31 && userAge <= 80:
-      console.log(`Always try to stay active both physically and mentally!`);
-      break;
-    case userAge < 20:
-      console.log(`Enjoy your early School years`);
-      break;
-    default:
-      console.log(`Sit back and relax!`);
-      break;
-  }
-};
-
-//tellMeYourAgeSwitch();
-console.log("----");
-
-console.log("---- Version 03");
-console.log("Using a ternary operator");
-let tellMeYourAgeTernary = () => {
-  let userAge = prompt("How old are you? - Using Ternary");
-  console.log(userAge);
-
-  userAge === "21"
-    ? console.log("You are 21 years old! - to be young")
-    : userAge >= 21 && userAge <= 30
-    ? console.log("Enjoy your 20's - :)")
-    : userAge >= 31 && userAge <= 80
-    ? console.log("Always try to stay active both physically and mentally!")
-    : userAge < 20
-    ? console.log("Enjoy your early School years")
-    : console.log("Sit back and relax!");
-};
-
-//tellMeYourAgeTernary();
-
-console.log("---- Version 03");
-console.log("Using if-else-if-else statements with typeChecking");
-let tellMeYourAgeWithValidation = () => {
-  let userAge = prompt("How old are you? - With Validation");
-
-  // Check if userAge is a number
-  if (isNaN(userAge)) {
-    console.log("Invalid input. Please enter a number.");
-    alert("Invalid input. Please enter a number.");
-    tellMeYourAgeWithValidation();
     return;
   }
-
-  // The userAge is a valid number, proceed with the logic
-  userAge = parseInt(userAge);
-
-  if (userAge === 21) {
-    console.log("You are 21 years old! - to be young");
-  } else if (userAge >= 21 && userAge <= 30) {
-    console.log("Enjoy your 20's - :)");
-  } else if (userAge >= 31 && userAge <= 80) {
-    console.log("Always try to stay active both physically and mentally!");
-  } else if (userAge < 20) {
-    console.log("Enjoy your early School years");
-  } else {
-    console.log("Sit back and relax!");
-  }
 };
 
-//tellMeYourAgeWithValidation();
+// tellMeYourAge();
+
+console.log("Using switch statements");
+const tellMeYourAgeSwitch = () => {
+  let userAge = prompt("How old are you?");
+  switch (true) {
+    case userAge <= 35:
+      console.log(`wow you are ${userAge}! that's young!`);
+      break;
+    case userAge > 35:
+    case userAge <= 60:
+      console.log(`wow you are ${userAge}! that's awesome!`);
+      break;
+    default:
+      return;
+  }
+};
+// tellMeYourAgeSwitch();
+
 console.log("----");
 
-console.log("Iteration 05");
-const calculationTest = () => {
-  let calculationString = prompt("what is 5 + 6?");
-  let calculationNumber = parseInt(calculationString);
-  if (calculationNumber === 11) {
-    console.log("Yay Correct");
-  } else {
-    console.log("Noooo, the answer is 11");
-  }
-  /*
+/*
     5)
     In this function we want to give the user a mathematical calculation to 
     give us the answer to. Something like prompt('What is 5 + 6?')  
@@ -174,9 +96,20 @@ const calculationTest = () => {
     If not, give the user the correct answer so they can learn. 'Noooo, the answer is 11'
     You choose if to use addition, subtraction, multiplication or division. 
     */
+console.log("Iteration 05");
+const calculationTest = (num1, num2) => {
+  let calculationRespons = prompt(`What is ${num1} * ${num2}`);
+  let rightAnswer = num1 * num2;
+  if (calculationRespons === rightAnswer) {
+    alert("Yay! Correct");
+  } else {
+    alert(`Noooo, the answer is ${rightAnswer}`);
+  }
 };
-calculationTest(10, 20);
+calculationTest(2, 4);
+
 console.log("----");
+
 /*
   6) **BONUS**
   Make the calculation machine we just made show random calculations everytime you invoke the function.
