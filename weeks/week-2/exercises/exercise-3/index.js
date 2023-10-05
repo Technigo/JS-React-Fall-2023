6876a6c8e3eee907499169acc0664058c6473f14
AntonellaMorittu <antonellamorittu@gmail.com>
2023-10-05 15:37:41 +0200

    Add solution to exercises - arrays and objects

--- weeks/week-2/exercises/exercise-3/index.js
+++ weeks/week-2/exercises/exercise-3/index.js
@@ -1,6 +1,5 @@
 // Loops & Objects Exercise
 
-
 const pokemons = [
   "Bulbasaur",
   "Ivysaur",
@@ -22,47 +21,180 @@ const pokemons = [
 // the pokemons from the pokemons array
 // invoke the function to see that it works
 
+console.log("Iteration 01");
+
+// Using Map
+console.log("---- Using Map() Array Method");
+pokemons.map((pokemon) => {
+  console.log(pokemon);
+});
+
+// Using Filter()
+console.log("---- Using Filter() Array Method");
+pokemons.filter((pokemon) => {
+  console.log(pokemon);
+});
+
+// Using For Each()
+console.log("---- Using forEach() Array Method");
+pokemons.forEach((pokemon) => {
+  console.log(pokemon);
+});
+
+// Using For  Loop()
+console.log("---- Using for Loop");
+let usingForLoop = () => {
+  for (let i = 0; i < pokemons.length; i++) {
+    console.log(pokemons[i]);
+  }
+};
+usingForLoop();
+
+// Using While Loop()
+console.log("---- Using doWhile Statement");
+let usingDoWhileLoop = () => {
+  let index = 0;
+  do {
+    console.log(pokemons[index]);
+    index++;
+  } while (index < pokemons.length);
+};
+usingDoWhileLoop();
+
+console.log("----");
+
+// Using ForOf Loop()
+console.log("---- Using forOf Loop");
+let usingForOf = () => {
+  for (const pokemon of pokemons) {
+    console.log(pokemon);
+  }
+};
+usingForOf();
+console.log("----");
+
 // 2)
 // Create an object for the pokemon Pikachu.
 // it could have properties such as name, element,
 // strength, color, amountOfLegs or something like that.
+console.log("Iteration 02");
+console.log("This iteration pertains to creating an object in code");
+let pokemon = {
+  name: "Pikachu",
+  age: 400,
+  category: "Mouse",
+  abilities: ["static electricity", "cute"],
+  strength: 32,
+  weight: "6.0 KG",
+};
 
+console.log("----");
 // 3)
 // console.log something like
 // 'Pikachu is a electricity pokemon with the strength of 32'
 // Selecting some of the information about your pikachu object.
+console.log("Iteration 03");
+console.log(
+  `${pokemon.name} is a ${pokemon.abilities[0]} pokemon with the strength of ${pokemon.strength}`
+);
+console.log("----");
 
 // 4)
 // Add the property stillToCatch: true to the Pikachu object.
+console.log("Iteration 04");
+pokemon.stillToCatch = true;
+console.table(pokemon);
+console.log("----");
 
 // 5)
 // change the value of the key strength in the Pikachu object.
+console.log("Iteration 05");
+pokemon.stillToCatch = false;
+console.table(pokemon);
+console.log("----");
 
 // 6)
 // Delete a property from the Pikachu object.
+console.log("Iteration 06");
+delete pokemon.weight;
+console.table(pokemon);
+console.log("----");
 
 // 7)
 // Fill up this array with a few more pokemons.
 // It is an array with objects!
 // I made some suggestions
 // but feel free to change as you want.
+console.log("Iteration 07");
+console.log(
+  "This iteration pertains to adding more pokemonsObjects to the array of objects"
+);
 const moreAboutPokemons = [
   {
     name: "Bulbasaur",
     element: "water",
-    strength: 13,
+    strength: 32,
   },
   {
     name: "Charmander",
     element: "fire",
     strength: 32,
   },
+  {
+    name: "Pikachu",
+    element: "earth",
+    strength: 35,
+  },
+  {
+    name: "Ditto",
+    element: "earth",
+    strength: 40,
+  },
 ];
 
 // 8)
 // Print out all the NAMES of the pokemons in this new array.
+console.log("Iteration 08");
+// Using forEach
+console.log("---- Using forEach() Array Method");
+moreAboutPokemons.forEach((pokemon) => {
+  console.log(`Pokemon name: ${pokemon.name}`);
+});
 
+// Using Map
+console.log("---- Using Map() Array Method");
+moreAboutPokemons.map((pokemon) => {
+  console.log(`Pokemon name: ${pokemon.name}`);
+});
+
+// Using Dot Notation
+console.log("---- Using Dot Notation");
+let dotNotationExample = `
+Pokemon names: 
+${moreAboutPokemons[0].name}
+${moreAboutPokemons[1].name}
+${moreAboutPokemons[2].name}
+${moreAboutPokemons[3].name}
+`;
+console.log(dotNotationExample);
+
+console.log("----");
 // 9)
 // Print out something like
 // 'Bulbasaur is a water pokemon with the strength of 32'
 // Do this for all the pokemons.
+console.log("Iteration 09");
+
+// Using Dot Notation for just one element
+console.log("---- Using Dot Notation for just one element");
+let bulbasaur = `${moreAboutPokemons[0].name} is a ${moreAboutPokemons[0].element} pokemon with the strength of ${moreAboutPokemons[0].strength}`;
+console.log(bulbasaur);
+
+// Using forEach to loop through array of objects
+console.log(
+  "---- Using forEach to showcase all pokemons within array of objects"
+);
+moreAboutPokemons.forEach((pokemon) => {
+  let text = `${pokemon.name} is a ${pokemon.element} pokemon with the strength of ${pokemon.strength}`;
+  console.log(text);
+});
--- weeks/week-2/exercises/exercise-4/index.js
+++ weeks/week-2/exercises/exercise-4/index.js
@@ -13,54 +13,159 @@ const pokemons = [
   "Metapod",
   "Weedle",
   "Pikachu",
-  "Pidgey"
+  "Pidgey",
 ];
 
+console.log("Iteration 01");
 const countThem = () => {
   // print out, in a sentence, how many pokemons I have.
   // like: "I have x pokemons!"
+  let mySentence = `I have ${pokemons.length} pokemons`;
+  console.log(mySentence);
 };
+countThem();
+console.log("----");
 
+console.log("Iteration 02");
+console.log("Using sort() method");
 const orderThem = () => {
   // order the pokemons alphabetically
+  let orderedPokemons = pokemons.sort((pokemonOne, pokemonTwo) =>
+    pokemonOne > pokemonTwo ? 1 : -1
+  );
+  console.table(orderedPokemons);
 };
+orderThem();
+console.log("----");
 
+console.log("Iteration 03");
+console.log("Using sort() method - reversed");
 const flipThem = () => {
   // reverse the order of the pokemons
+  // order the pokemons alphabetically
+  let reversedPokemons = pokemons.sort((pokemonOne, pokemonTwo) =>
+    pokemonOne > pokemonTwo ? -1 : 1
+  );
+  console.table(reversedPokemons);
 };
+flipThem();
+console.log("----");
 
+console.log("Iteration 04");
 const makeThemBig = () => {
   // print the pokemons in UPPERCASE letters
+  pokemons.forEach((pokemon) => console.log(pokemon.toUpperCase()));
 };
+makeThemBig();
+console.log("----");
 
+console.log("Iteration 05");
+console.log("Using filter() method + string method startsWith()");
 const onlyTheBs = () => {
   // only print the pokemons that starts with B
+  let filteredPokemons = pokemons.filter((pokemon) => pokemon.startsWith("B"));
+  console.log(filteredPokemons);
+  // filteredPokemons.forEach((pokemon) => console.log(pokemon));
 };
+onlyTheBs();
 
+console.log("Using forEach() method + string method startsWith()");
+const onlyTheBsTwo = () => {
+  // only print the pokemons that starts with B
+  pokemons.forEach((pokemon) => {
+    if (pokemon.startsWith("B")) {
+      console.log(pokemon);
+    }
+  });
+};
+onlyTheBsTwo();
+console.log("----");
+
+console.log("Iteration 06");
 const notTheCs = () => {
   // remove all pokemons that starts with C
+  let filtered = pokemons.filter((pokemon) => !pokemon.startsWith("C"));
+  filtered.forEach((pokemon) => console.log(pokemon));
 };
+notTheCs();
+console.log("----");
 
+console.log("Iteration 07");
 const nameAndIdThanks = () => {
   // print out name and index of all pokemons
   // like: number x - Squirtle
+  // Using forEach
+  console.log("Using forEach() method");
+  pokemons.forEach((pokemon, id) => {
+    let myString = `Number ${id} - ${pokemon}`;
+    console.log(myString);
+  });
+  // Using forLoop
+  console.log("---");
+  console.log("Using map() method");
+  pokemons.map((pokemon, id, position) => {
+    let myString = `Number ${id} - ${pokemon}`;
+    console.log(myString);
+  });
 };
+nameAndIdThanks();
+console.log("----");
 
-const catchPokemon = name => {
+console.log("Iteration 08");
+const catchPokemon = (name) => {
   // add a pokemon with a name of your choice to the list,
   // print the list so you see its there.
+  pokemons.push(name);
+  console.log(`${name} has been caught and added to the pokemons array!`);
 };
+catchPokemon("Raichu");
+console.log(pokemons);
+console.log("----");
 
-const didICatchIt = name => {
+console.log("Iteration 09");
+const didICatchIt = (name) => {
   // check the pokemons to see if a specific pokemon is in the array
+  // pokemons.includes(name);
+  if (pokemons.includes(name)) {
+    console.log(`Cool, I already caught this pokemon: ${name}`);
+  } else {
+    console.log(`You have not catched this pokemon '${name}' yet, go for it!`);
+  }
 };
+didICatchIt("Mewtwo");
+console.log("----");
 
+console.log("Iteration 10");
 const addInThirdPlace = () => {
   // add the pokemon "Clefairy" in the third place of the array
   // print the list so you see its there.
+  // Explanation
+  //The addPokemonToThirdPosition function uses the splice method to insert the Pokémon at the 3rd position, with a confirmation message logged afterwards. By modifying the pokemon variable inside the function, different Pokémon names can be added. The slice method, with arguments 2 and 0, extracts a portion of the pokemons array starting from the 2nd position, creating an empty slice that doesn't include any elements after the starting index.
+  const pokemon = "Clefairy";
+  pokemons.splice(2, 0, pokemon);
+  console.log(
+    `${pokemon} has been added to the 3rd position of the pokemons array!`
+  );
 };
+addInThirdPlace();
+console.log(pokemons);
+console.log("----");
 
+console.log("Iteration 11 ");
+console.log("Bonus");
 // ***BONUS***
 const theLongestName = () => {
   // find the pokemon with the longest name
+  const sortedPokemons = pokemons.sort((a, b) => b.length - a.length);
+  longestNamePokemon = sortedPokemons[0];
+
+  // Get the character count of the longestNamePokemon
+  const characterCount = longestNamePokemon.length;
+  console.log(longestNamePokemon, characterCount);
+  console.log(
+    `The pokemon with the longest name is ${longestNamePokemon} with ${characterCount} characters under his name`
+  );
 };
+theLongestName();
+
+console.log("----");
